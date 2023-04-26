c003129873cbdd73986e5cd757d170a01df90e55
Niloo <Niloo@Niloufars-MacBook-Pro.local>
2023-04-26 13:29:56 -0400

    Remove consol logs

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,39 +6,34 @@ import classes from "./Home.module.css";
 const Home = () => {
   const homeRef = useRef();
   const profileRef = useRef();
-  
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       let homeWidth = homeRef.current.getBoundingClientRect().width + 5;
-      console.log(homeWidth);
       if (1025 < homeWidth) {
-        console.log("large");
         homeRef.current.style.backgroundPositionY =
           homeRef.current.getBoundingClientRect().top * 0.5 - 250 + "px";
       } else if (768 < homeWidth && homeWidth < 1025) {
-        console.log("small");
         homeRef.current.style.backgroundPositionY =
           homeRef.current.getBoundingClientRect().top * 0.23 + "px";
       } else if (homeWidth < 769) {
-        console.log("ipad");
         homeRef.current.style.backgroundPositionY =
-          homeRef.current.getBoundingClientRect().top * -0.5  + "px";
+          homeRef.current.getBoundingClientRect().top * -0.5 + "px";
       } else {
-        console.log('large');
         homeRef.current.style.backgroundPositionY =
           homeRef.current.getBoundingClientRect().top * 0.5 + "px";
       }
     });
   }, []);
-  const scrollToMore = () =>{
+  const scrollToMore = () => {
     const screenWidth = window.innerWidth;
-    const homeHeight = document.getElementById('home').clientHeight;
-    if(screenWidth > 768){
-      window.scrollTo({top: homeHeight,behavior:"smooth"})
-    } else{
-      window.scrollTo({top: homeHeight - 72,behavior:"smooth"})
+    const homeHeight = document.getElementById("home").clientHeight;
+    if (screenWidth > 768) {
+      window.scrollTo({ top: homeHeight, behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: homeHeight - 72, behavior: "smooth" });
     }
-  }
+  };
   return (
     <div className={classes.home} id="home" ref={homeRef}>
       <Row className={classes["home-content"]}>
@@ -54,7 +49,9 @@ const Home = () => {
             me with the opportunity to work on multiple projects simultaneously,
             and to work closely with cross-functional teams.
           </div>
-          <button className={classes.more} onClick={scrollToMore}>More Info</button>
+          <button className={classes.more} onClick={scrollToMore}>
+            More Info
+          </button>
         </div>
       </Row>
     </div>
